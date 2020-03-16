e849efec4760800562097ead887c5f2e2fc79484
sapoorv70 <apoorv-lagwal@live.nmit.ac.nz>
2020-03-17 11:18:15 +1300

    lab-3 done

---
+++ idex.js
@@ -0,0 +1,85 @@
+/*const person = 'John Smith';
+switch (person) {
+  case 'John Smith':
+    let marks = '98';
+    if (marks <= 39) {
+        console.log("E Fail Grade")
+    } else if (marks <= 49) {
+        console.log("D Fail Grade")
+    } else if (marks <= 54) {
+        console.log("C Pass")
+    } else if (marks <= 59) {
+        console.log("C+ Pass")
+    } else if (marks <=64) {
+        console.log("B- Pass With Merit")
+    } else if (marks <= 69) {
+        console.log("B Pass Wiht Merit")
+    } else if (marks <= 74) {
+        console.log("B+ Pass With Merit")
+    } else if (marks <= 79) {
+        console.log("A- Pass With Distinction")
+    } else if (marks <= 84) {
+        console.log("A Pass With Distinction")
+    } else if (marks <= 100) {
+        console.log("A+ Pass With Distinction")
+    } else {
+        console.log("Pass With Distinction")
+    }
+    console.log('John Smith A+ Pass');
+    break; // final marking scheme for student (John Smith)
+    case 'Addie Lilley':
+    marks = '67';
+    if (marks <= 39) {
+        console.log("E Fail Grade")
+    } else if (marks <= 49) {
+        console.log("D Fail Grade")
+    } else if (marks <= 54) {
+        console.log("C Pass")
+    } else if (marks <= 59) {
+        console.log("C+ Pass")
+    } else if (marks <=64) {
+        console.log("B- Pass With Merit")
+    } else if (marks <= 69) {
+        console.log("B Pass Wiht Merit")
+    } else if (marks <= 74) {
+        console.log("B+ Pass With Merit")
+    } else if (marks <= 79) {
+        console.log("A- Pass With Distinction")
+    } else if (marks <= 84) {
+        console.log("A Pass With Distinction")
+    } else if (marks <= 100) {
+        console.log("A+ Pass With Distinction")
+    } else {
+        console.log("Pass With Distinction")
+    }
+    console.log('Addie Lilley B+ Pass');
+    break; // final marking scheme for student (Addie Lilly)
+    case 'Shaun Myers':
+        marks = '34';
+        if (marks <= 39) {
+            console.log("E Fail Grade")
+        } else if (marks <= 49) {
+            console.log("D Fail Grade")
+        } else if (marks <= 54) {
+            console.log("C Pass")
+        } else if (marks <= 59) {
+            console.log("C+ Pass")
+        } else if (marks <=64) {
+            console.log("B- Pass With Merit")
+        } else if (marks <= 69) {
+            console.log("B Pass Wiht Merit")
+        } else if (marks <= 74) {
+            console.log("B+ Pass With Merit")
+        } else if (marks <= 79) {
+            console.log("A- Pass With Distinction")
+        } else if (marks <= 84) {
+            console.log("A Pass With Distinction")
+        } else if (marks <= 100) {
+            console.log("A+ Pass With Distinction")
+        } else {
+            console.log("Pass With Distinction")
+        }
+        console.log('Shaun Myers B+ Pass');
+        break; // final marking scheme for student (Shaun Myers)
+    }*/
+
---
+++ index.js
@@ -0,0 +1,46 @@
+const marks = '98';
+switch (true) {
+case (marks <=100 && marks >= 86):
+    console.log('A+');
+    console.log('Pass With Distinction');
+    break;
+case (marks <=85 && marks >= 81):
+    console.log('A');
+    console.log('Pass With Distinction');
+    break;
+case (marks <=80 && marks >= 76):
+    console.log('A-');
+    console.log('Pass With Distinction');
+    break;
+case (marks <=75 && marks >= 71):
+    console.log('B+');
+    console.log('Pass With Merit');
+    break;
+case marks <=70 && marks >= 66:
+    console.log('B');
+    console.log('Pass With Merit');
+    break;
+case (marks <=65 && marks >= 61):
+    console.log('B-');
+    console.log('Pass With Merit');
+    break;
+case (marks <=60 & marks >= 56):
+    console.log('C+');
+    console.log('Pass');
+    break;
+case (marks <=55 && marks >= 50):
+    console.log('C');
+    console.log('Pass');
+    break;
+case (marks <=49 && marks >= 40):
+    console.log('D Fail Grade');
+    console.log('Pass With Distinction');
+    break;
+case (marks <=39 && marks >= 0):
+    console.log('E');
+    console.log('Fail Grade');
+    break;
+default:
+    console.log('fail');
+    break;
+}
